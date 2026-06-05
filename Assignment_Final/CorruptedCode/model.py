7433f6c90f6acb69e5f709c525707cc05c0a3448
mgswiss15 <mgswiss15@gmail.com>
2026-06-05 09:23:01 +0200

    copy clean to corrupted

--- Assignment_Final/CorruptedCode/model.py
+++ Assignment_Final/CorruptedCode/model.py
@@ -13,7 +13,7 @@ class ResBlock(nn.Module):
             out_channels = channels * 2
             stride = 2
             self.shortcut = nn.Sequential(
-                nn.Conv2d(channels, out_channels, kernel_size=1, stride=1, bias=False),
+                nn.Conv2d(channels, out_channels, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_channels)
             )
         self.conv1 = nn.Conv2d(channels, out_channels, kernel_size=3, padding=1, stride=stride, bias=False)
@@ -33,8 +33,8 @@ class ResBlock(nn.Module):
 
 class ResNet(nn.Module):
     def __init__(self, in_channels, num_classes):
-        
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1, bias=False)
+        super().__init__()      
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=32, kernel_size=3, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(num_features=32)
         self.relu = nn.ReLU(inplace=True)
 
@@ -46,10 +46,8 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         x = self.relu(self.bn1(self.conv1(x)))
-
         x = self.resblock1(x)
         x = self.resblock2(x)
-
         x = self.avgpool(x).flatten(1)
         x = self.fc(x)
         return x
