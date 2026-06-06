vgg16 and data leakackage corrected

Assignment_Final/CleanCode/models.py
@@ -12,7 +12,7 @@ class VGGBlock(nn.Module):
 
     C configuration from Simonyan & Zisserman's VGG paper.
     """
-    def __init__(self, in_channels, out_channels, num_convs):
+    def __init__(self, in_channels, out_channels, num_convs, padding=1):
         super().__init__()
         layers = []
         current_in_channels = in_channels
@@ -103,8 +103,8 @@ class AlexNet(nn.Module):
         return self.classifier(x)
 
 
-class VGG(nn.Module):
-    """VGG in C configuration of Simonyan & Zisserman, (2014) adapted for smaller inputs."""
+class VGG16(nn.Module):
+    """VGG16 in C configuration of Simonyan & Zisserman, (2014) adapted for smaller inputs."""
     def __init__(self, in_channels, num_classes, **kwargs):
         super().__init__()
 
@@ -119,11 +119,13 @@ class VGG(nn.Module):
         )
         
         self.classifier = nn.Sequential(
-            nn.Linear(2048, 512),
+            nn.Linear(2048, 1024),
             nn.ReLU(inplace=True),
             nn.Dropout(p=drop_rate),
-            nn.Linear(512, num_classes),
-            nn.Softmax(dim=1)
+            nn.Linear(1024, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=drop_rate),
+            nn.Linear(512, num_classes)
         )
 
     def forward(self, x):

Assignment_Final/CorruptedCode/data.py
@@ -10,9 +10,18 @@ from torch.utils.data import TensorDataset, DataLoader
 def get_loaders(data, data_path, batch_size):
     d_path = Path(data_path) / f"{data}_data.pt"
     data_dict = torch.load(d_path)
+
+    total_samples = data_dict['train_images'].shape[0]
+    val_size = int(total_samples * val_split)
+    val_start = total_samples - val_size
+
+    train_data = data_dict['train_images']
+    train_labels = data_dict['train_labels']
+    val_data = data_dict['train_images'][val_start:]
+    val_labels = data_dict['train_labels'][val_start:]
     
-    train_dataset = TensorDataset(data_dict['train_images'], data_dict['train_labels'])
-    val_dataset = TensorDataset(data_dict['train_images'], data_dict['train_labels'])
+    train_dataset = TensorDataset(train_data, train_labels)
+    val_dataset = TensorDataset(val_data, val_labels)
     test_dataset = TensorDataset(data_dict['test_images'], data_dict['test_labels'])
     
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)