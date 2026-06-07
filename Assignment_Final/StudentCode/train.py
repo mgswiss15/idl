d603da0983858c18035d09697c7f1001b7f5d411
mgswiss15 <mgswiss15@gmial.com>
2026-06-07 08:17:17 +0200

    assignment final activatin_str

--- Assignment_Final/StudentCode/train.py
+++ Assignment_Final/StudentCode/train.py
@@ -22,7 +22,7 @@ def main():
     train_loader, val_loader, _ = get_loaders(data=config["DATA"], data_path=config["DATA_PATH"], batch_size=config["BATCH_SIZE"])
 
     model_class = getattr(models, config["MODEL"])
-    model = model_class(in_channels=config["CHANNELS"], num_classes=config["NUM_CLASSES"], drop_rate=0.99).to(device)
+    model = model_class(in_channels=config["CHANNELS"], num_classes=config["NUM_CLASSES"], drop_rate=0.99, activation_str=None).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=config["LEARNING_RATE"])
 
